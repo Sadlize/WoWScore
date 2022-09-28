4f7b3026ed8cf78009f2f85fd71ddb2f7605e17b
Sadlize <sadlize@mail.ru>
2022-09-28 05:33:39 +0300

    [Update]: Change input logic to prevent component rerenders

--- src/Components/Inputs/CalculatorInput/CalculatorInput.jsx
+++ src/Components/Inputs/CalculatorInput/CalculatorInput.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import clsx from "clsx";
 import {calcPointsForKeyLevel} from "../../../utils/Calculator/Calculator";
 
@@ -10,25 +10,39 @@ const CalculatorInput = (props) => {
     const key = props?.index
     const week = props?.week
 
+    const [input, setInput] = useState('')
+    const isDifferent = (value) => {
+        value = value.replace(/[^0-9]/g, '')
+        if (input === value) {
+            return 0 //same
+        } else {
+            return 1 //different
+        }
+    }
+
     return (
         <div id={key}>
             <input
+                value={input}
                 placeholder={placeholder}
                 onFocus={(e) => e.target.placeholder = ''}
                 onBlur={(e) => e.target.placeholder = placeholder}
-                type='number' autoComplete="off"
+                type='text' autoComplete="off" maxLength='2'
                 className={clsx({'grayscale100': scorePerDungeon[key][week] === 0}, 'inp')}
-
-                onChange={event =>
-                    setScorePerDungeon(prevState => (
-                        {
-                            ...prevState,
-                            [key]: {
-                                ...prevState[key],
-                                [week]: calcPointsForKeyLevel(+event.target.value)
-                            }
+                onChange={e => {
+                    const value = e.target.value
+                    if (value.length <= e.target.maxLength) {
+                        if (isDifferent(value)) {
+                            setInput(value)
+                            setScorePerDungeon(prevState => (
+                                {
+                                    ...prevState,
+                                    [key]: {...prevState[key], [week]: calcPointsForKeyLevel(+value)}
+                                }
+                            ))
                         }
-                    ))}
+                    }
+                }}
                 // onContextMenu={(e) => {
                 //     e.preventDefault();
                 // }}
