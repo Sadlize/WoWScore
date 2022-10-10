6f962ef1f6e0e6315c4c9722b7650197a2212cda
Sadlize <sadlize@mail.ru>
2022-10-10 08:37:06 +0300

    [Moved]: thumbnail icon links to a separate component.

--- src/Components/UI/ScoreCalculator/ScoreCalculator.jsx
+++ src/Components/UI/ScoreCalculator/ScoreCalculator.jsx
@@ -9,6 +9,7 @@ import apiFunctionHandler from "../../../utils/apiFunctionHandler";
 import {useFetching} from "../../../hooks/useFetching";
 import _ from "lodash";
 import UpgradeStars from "./UpgradeStars";
+import ScoreCalculatorLinks from "./ScoreCalculatorLinks";
 
 const ScoreCalculator = () => {
     const currentDungeons = ['STRT', 'GMBT', 'YARD', 'WORK', 'ID', 'GD', 'LOWR', 'UPPR']
@@ -70,41 +71,11 @@ const ScoreCalculator = () => {
                         Dungeons inputs support MRB click.
                     </Tooltip>
                 </TooltipGroup>
-                {playerInfo?.data
-                    ? <div style={{display: "inline-flex"}}>
-                        <a
-                            href={`https://worldofwarcraft.com/en-gb/character/${playerInfo?.data?.region}/howling-fjord/${playerInfo?.data?.name}`}
-                            title="Armory Profile" className="profile-links wowArmory"
-                            target="_blank"
-                            rel="noreferrer"
-                        > </a>
-
-                        <a
-                            href={playerInfo?.data?.profile_url}
-                            title="RaiderIO Profile" className="profile-links raiderIO"
-                            target="_blank"
-                            rel="noreferrer"
-                        > </a>
-                        {!isPlayerInfoLoading &&
-                            <img
-                                src={playerInfo?.data?.thumbnail_url}
-                                className='guest'
-                                alt={'Character thumbnail'}
-                                style={{borderRadius: '50%'}}
-                            />
-                        }
-                        <a
-                            href={`/`}
-                            title="WarcraftLogs Profile" className="profile-links warcraftLogs"
-                            rel="noreferrer"
-                        > </a>
-                        <a
-                            href={`/`}
-                            title="Character sim on RaidBots" className="profile-links raidBots"
-                            rel="noreferrer"
-                        > </a>
-                    </div>
-                    : <></>
+                {playerInfo?.data &&
+                    <ScoreCalculatorLinks
+                        playerInfo={playerInfo}
+                        isPlayerInfoLoading={isPlayerInfoLoading}
+                    />
                 }
                 <p className='CalcScore'>{sumDungeonScoreValues}</p>
                 {currentDungeons.map((index) => (
