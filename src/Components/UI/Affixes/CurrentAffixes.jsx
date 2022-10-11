1b52e452572171bff776bf4ea2fe2cc243fe045e
Sadlize <sadlize@mail.ru>
2022-10-11 09:27:50 +0300

    [Add]: gap between affixes icons.

--- src/Components/UI/Affixes/CurrentAffixes.jsx
+++ src/Components/UI/Affixes/CurrentAffixes.jsx
@@ -5,55 +5,57 @@ import RaiderIO from "../../../API/RaiderIO";
 
 const CurrentAffixes = () => {
 
-    /**
-     *  Current Week CurrentAffixes
-     */
-    const [currentAffixes, setCurrentAffixes] = useState([])
-    const [fetchCurrentAffixes, isCurrentAffixesLoading] = useFetching(useCallback(async () => {
-        const response = await RaiderIO.getCurrentAffixes()
-        setCurrentAffixes(response)
-    }, []))
+  /**
+   *  Current Week CurrentAffixes
+   */
+  const [currentAffixes, setCurrentAffixes] = useState([])
+  const [fetchCurrentAffixes, isCurrentAffixesLoading] = useFetching(useCallback(async () => {
+    const response = await RaiderIO.getCurrentAffixes()
+    setCurrentAffixes(response)
+  }, []))
 
-    /**
-     *  Current Week Dates
-     */
-    const [currentWeekPeriod, setCurrentWeekPeriod] = useState('')
-    const [fetchCurrentWeekPeriod] = useFetching(useCallback(async () => {
-        const response = await RaiderIO.getCurrentWeekPeriod()
-        setCurrentWeekPeriod(response)
-    }, []))
+  /**
+   *  Current Week Dates
+   */
+  const [currentWeekPeriod, setCurrentWeekPeriod] = useState('')
+  const [fetchCurrentWeekPeriod] = useFetching(useCallback(async () => {
+    const response = await RaiderIO.getCurrentWeekPeriod()
+    setCurrentWeekPeriod(response)
+  }, []))
 
-    useEffect(() => {
-        fetchCurrentAffixes()
-        fetchCurrentWeekPeriod()
-    }, [fetchCurrentAffixes, fetchCurrentWeekPeriod])
+  useEffect(() => {
+    fetchCurrentAffixes()
+    fetchCurrentWeekPeriod()
+  }, [fetchCurrentAffixes, fetchCurrentWeekPeriod])
 
-    return (
-        <div>
-            <div className='content-block'>
-                {isCurrentAffixesLoading
-                    ? <span>Loading current affixes...</span>
-                    : <>
-                        <p>{currentWeekPeriod}</p>
-                        {currentAffixes.map((currentAffixes) => (
-                            <a
-                                key={currentAffixes?.id}
-                                href={'https://wowhead.com/affix=' + currentAffixes?.id}
-                                target="_blank"
-                                rel='noreferrer'
-                            >
-                                <img
-                                    src={"https://wow.zamimg.com/images/wow/icons/large/" + currentAffixes?.icon + ".jpg"}
-                                    alt={currentAffixes?.name}
-                                />
-                            </a>
-                        ))}
-                        <p>{currentAffixes.map(item => item?.name).join(', ')}</p>
-                    </>
-                }
+  return (
+      <div className='content-block'>
+        {isCurrentAffixesLoading
+          ? <span>Loading current affixes...</span>
+          : <>
+            <p>{currentWeekPeriod}</p>
+            <div className="Affixes">
+              {currentAffixes.map((currentAffixes) =>
+                (
+                  <a
+                    key={currentAffixes?.id}
+                    href={'https://wowhead.com/affix=' + currentAffixes?.id}
+                    target="_blank"
+                    rel='noreferrer'
+                  >
+                    <img
+                      src={"https://wow.zamimg.com/images/wow/icons/large/" + currentAffixes?.icon + ".jpg"}
+                      alt={currentAffixes?.name}
+                    />
+                  </a>
+                )
+              )}
             </div>
-        </div>
-    );
+            <p>{currentAffixes.map(item => item?.name).join(', ')}</p>
+          </>
+        }
+      </div>
+  );
 };
 
 export default CurrentAffixes;
